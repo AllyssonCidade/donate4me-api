0839c82e2070faa358877c610d6c58605c62e30e
AllyssonCidade <allyssoncidade@gmail.com>
2025-01-28 17:02:40 -0300

    feat

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
